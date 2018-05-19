f9f659de2cc7b16e663d5332f2cabba27f71bd08
Jacob Killelea <jkillelea@protonmail.ch>
2018-05-18 20:14:52 -0700

    updates

--- assets/2018-05-17/jlplot.jl
+++ assets/2018-05-17/jlplot.jl
@@ -1,14 +1,19 @@
 using Plots
 plotly()
 
-humid = readdlm("bathroom_humidity")
-gui(plot(humid, 
-        ylabel = "Relative Humidity (%)", 
-        xlabel = "measurement number",
-        leg = false))
+humid = readdlm("humidity")
+temp = readdlm("temperature")
+rssi = readdlm("rssi")
 
-temp = readdlm("bathroom_temperature")
-gui(plot(temp, 
-        ylabel = "Temperature (deg C)", 
-        xlabel = "measurement number",
-        leg = false))
+plot(humid, # temperature and humidity
+     ylabel = "Relative Humidity (%)", 
+     xlabel = "measurement number",
+     size   = [1600, 900],
+     label  = "Humidity")
+gui(plot!(temp, label = "Temperature"))
+
+gui(plot(rssi,  # recieved signal strength
+         ylabel = "dBm", 
+         xlabel = "measurement number",
+         size   = [1600, 900],
+         label  = "RSSI"))
--- assets/2018-05-17/process.sh
+++ assets/2018-05-17/process.sh
@@ -1,2 +1,9 @@
-cat log.txt | grep 368 | grep temp | awk '{split($0, a, "|"); print a[2]}' > bathroom_temperature
-cat log.txt | grep 368 | grep humid | awk '{split($0, a, "|"); print a[2]}' > bathroom_humidity
+#!/bin/sh
+
+chip=368172
+log="$1" || "log.txt"
+
+for param in humidity temperature rssi; do
+  topic=$chip/$param
+  cat $log | grep $topic | awk '{split($0, a, "|"); print a[2]}' > $param
+done
